0496bfcdd859048f9e4a2a15c01b0639449b5bae
Akosipotpot24 <tongcohermogenes@gmail.com>
2026-02-02 18:34:15 +0800

    revised project for library attendance system

---
+++ database/migrations/2026_02_01_060844_create_attendances_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_number');
+            $table->string('student_name');
+            $table->string('library_location');
+            $table->string('attendance_date');
+            $table->string('grade_level');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
